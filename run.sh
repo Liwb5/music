87b0538ccbe0bcb5215c04c0e049f8ec8de76e16
asus_wsl <liwb5@foxmail.com>
2019-01-06 11:18:27 +0800

    add some songs

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-song='HuiMengYouXian'
-X='G'
+song='YiCiJiuHao'
+X='D'
 C='C'
 #direction=None
 python inflexion.py -f ./songs/${song}_${X}.txt -s ./songs/${song}_${C}.txt -x ${X} -c ${C} #-d ${direction} 
